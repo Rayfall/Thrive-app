30007cada9a8bf4a6fd7fc23663f7ce5b5209c79
Rayfall <atgrant23@gmail.com>
2020-05-12 13:38:20 -0500

    Finished logic for displaying tasks

--- FE/src/Pages/Home.js
+++ FE/src/Pages/Home.js
@@ -5,22 +5,30 @@ import { DataContext } from "../App";
 export default function Home() {
     const data = useContext(DataContext);
     let tasks = data.allTasks.map((task,index)=>{
-        let items = task.items.map((item,index) => {
-            let subitems = item.subtasks.map((task,index)=> {
-                return <li key={index}>{task.subtask}</li>
+        if(task.items == null){
+            return <h3 key={index}>{task.title}</h3>;
+        } else {
+            let items = task.items.map((item,index) => {
+                if(item.subtasks == null){
+                    return <li key={index}>{item.item}</li>
+                } else {
+                    let subitems = item.subtasks.map((task,index)=> {
+                        return <li key={index}>{task.subtask}</li>
+                    })
+                    return (
+                        <li key={index}>{item.item}
+                            <ul>{subitems}</ul>
+                        </li>
+                    )
+                }
             })
             return (
-                <li key={index}>{item.item}
-                    <ul>{subitems}</ul>
-                </li>)
-        })
-        return (
-            <h3 key={index}>{task.title}
-                <ul>{items}</ul>
-            </h3>
-        );
+                <h3 key={index}>{task.title}
+                    <ul>{items}</ul>
+                </h3>
+            );
+        }        
     });
-    //console.log(tasks.allTasks[0]);
     return (
     <>
        <h1>Home Page</h1>
