71b5b90bae4e7635c748705bbcf10277ff3b10a7
Rayfall <atgrant23@gmail.com>
2020-05-12 10:59:31 -0500

    Switched around the css for footer/header

--- FE/src/Components/Footer.js
+++ FE/src/Components/Footer.js
@@ -1,9 +1,9 @@
 import React from "react";
-import '../Styles/footer.css';
+import './footer.css'
 
 export default function Footer() {
     return (
-    <div className="background">
+    <div className="background-footer">
         <h5>Umbra Falls LLC</h5>
         <h5>Copyright 2020</h5>
     </div>
