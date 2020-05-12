3518292a4b3302ae2dadcceed1a4a00b7c3ffd76
Rayfall <atgrant23@gmail.com>
2020-05-12 12:06:52 -0500

    Decent login page started

--- FE/src/Pages/Login.js
+++ FE/src/Pages/Login.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Authentication coming soon!');
+        console.log("Email: ", email);
+        console.log("Password: ", password);
+    }
+
     return (
         <div>
-            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit}>
+                <h1>Login Page</h1>
+                <input 
+                    type="email"
+                    name="email"
+                    value = {email}
+                    onChange={e => setEmail(e.target.value)} 
+                    placeholder="Enter Email"
+                    required
+                />
+                <input
+                    type="password"
+                    name="password"
+                    value = {password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Enter Password"
+                    required
+                />
+                <input type="submit" value="Submit"/>
+            </form>
         </div>
     )
 }
 
 // Login.jsx
 /*
+
+<button onClick={handleSubmit}>ENTER</button>
+
 import React, { Component } from 'react';
 
 export default class Login extends Component {
