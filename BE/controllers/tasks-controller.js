caa5388e11ce5516eafda7fd249a34456b72489f
Rayfall <atgrant23@gmail.com>
2020-05-15 11:21:29 -0500

    Task backend update

--- BE/controllers/tasks-controller.js
+++ BE/controllers/tasks-controller.js
@@ -30,6 +30,7 @@ const createTask = (req, res) => {
 const updateTaskById = (req, res) => {
     console.log( req.params.id);
     Task.updateOne({_id: req.params.id}).then(task => {
+        console.log("This is task: ", task)
         task.title.push(task._id)
         task.isPriority.push(task._id)
         task.items.push(task._id)
